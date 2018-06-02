bdfc0f4aad1fe179991fd69cabd2a41c79dde5de
Wei-Tsung Su <ellington.su@gmail.com>
2018-06-03 00:50:20 +0800

    update setup

--- jni/tw/edu/au/csie/ucan/beebit/setup.java
+++ jni/tw/edu/au/csie/ucan/beebit/setup.java
@@ -2,8 +2,8 @@ package tw.edu.au.csie.ucan.beebit;
 
 class setup {
 	public static void main(String[] args) {
-		cpabeJNI bee = new cpabeJNI();
-		if(bee.setup("./pubKey", "./mstKey") == -1){
+		cpabeJNI beebit = new cpabeJNI();
+		if(beebit.setup("./pubKey", "./mstKey") == cpabeJNI.EXIT_FAILURE){
     			System.out.print("SETUP FAILED(JNI)\n");
 		}
     		System.out.print("SETUP SUCCESS(JNI)\n");
